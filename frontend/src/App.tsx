db14018307db72273a6f2e7a3aba9edc5154c0e2
Manfred Fong <mafong081102@gmail.com>
2026-01-13 18:54:47 -0500

    scafolded project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full h-full text-5xl text-red-500">
+      Hello World
+    </div>
+  )
+}
+
+export default App
